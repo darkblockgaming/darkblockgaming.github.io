ba71b064a74d533321b7e8c11c2a334dc6623547
DarkBlock Gaming <dbgkit@gmail.com>
2025-05-22 14:50:15 +0530

    styleandjs

---
+++ script.js
@@ -0,0 +1,29 @@
+function toggleMode() {
+  document.body.classList.toggle('light-mode');
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+    document.body.classList.add('light-mode');
+  }
+});
+
+
+
+
+const btn = document.getElementById('toggle-md');
+const container = document.getElementById('markdown-container');
+
+
+
+fetch("../../markdowns/ReplayCraft.md")
+  .then(res => res.text())
+  .then(md => {
+    container.innerHTML = marked.parse(md);
+  });
+
+btn.addEventListener('click', () => {
+  const isCollapsed = container.classList.toggle('expanded');
+  container.classList.toggle('collapsed', !isCollapsed);
+  btn.textContent = isCollapsed ? 'Show Less' : 'Show More';
+});
